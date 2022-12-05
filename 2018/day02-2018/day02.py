28e6064794da36bda4658697f550ec86573f9b88
Alexander Drakon <drakon.a.v@gmail.com>
2022-12-05 15:51:53 +0300

    2018 days 2-3

--- 2018/day02-2018/day02.py
+++ 2018/day02-2018/day02.py
@@ -1,14 +1,17 @@
-ids = []
+
 ABC = 'abcdefghijklmnopqrstuvwxyz'
+
+ids = []
 with open('day02input.txt', 'r') as inpfile:
     while True:
         line = inpfile.readline().strip()
         if line == '':
             break
         ids.append(line)
-print(ids)
+
 count2 = 0
-count3 = 0 
+count3 = 0
+
 for id in ids:
     is2 = False
     is3 = False
@@ -22,11 +25,19 @@ for id in ids:
         if is2 and is3:
             break
     if is2:
-        count2 +=1
+        count2 += 1
     if is3:
-        count3 +=1
+        count3 += 1
 
-checksum = count2*count3
+checksum = count2 * count3
 print('Checksum is', checksum)
 input()
 
+for id1 in ids:
+    for id2 in ids:
+        count = 0
+        for i in range(len(id1)):
+            if id1[i] != id2[i]:
+                count += 1
+        if count == 1:
+            print(id1, id2)
