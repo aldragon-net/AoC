1d199f23b69291aa6152e7a1c49f7028c0f34824
Alexander Drakon <drakon.a.v@gmail.com>
2022-12-08 12:24:04 +0300

    day10-2016

--- 2016/day10-2016/day10.py
+++ 2016/day10-2016/day10.py
@@ -3,7 +3,7 @@ with open('input_day10.txt', 'r') as f:
 
 
 bots = [[] for _ in range(256)]
-outputs = [None] * 20
+outputs = [None] * 24
 bot_links = [[None, None] for _ in range(256)]
 
 for line in lines:
@@ -12,4 +12,32 @@ for line in lines:
         bots[int(words[-1])].append(int(words[1]))
         continue
     if words[0] == 'bot':
-        pass
+        number = int(words[1])
+        low = int(words[6])
+        if words[5] == 'output':
+            low += 1000
+        high = int(words[11])
+        if words[10] == 'output':
+            high += 1000
+        bot_links[number] = [low, high]
+
+while True:
+    i = 0
+    for i in range(len(bots)):
+        if len(bots[i]) == 2:
+            if 17 in bots[i] and 61 in bots[i]:
+                print(i)
+            break
+    else:
+        break
+    if bot_links[i][0] < 1000:
+        bots[bot_links[i][0]].append(min(bots[i]))
+    else:
+        outputs[bot_links[i][0] - 1000] = min(bots[i])
+    if bot_links[i][1] < 1000:
+        bots[bot_links[i][1]].append(max(bots[i]))
+    else:
+        outputs[bot_links[i][1] - 1000] = max(bots[i])
+    bots[i] = []
+
+print(outputs[0] * outputs[1] * outputs[2])
